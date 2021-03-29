cb111e96c77a2630f2cb68786a81c00c4a7a4692
Polina <rabarbrablad@gmail.com>
2021-03-29 11:52:12 +0000

    Added error handlers

--- delivery_api/__init__.py
+++ delivery_api/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, jsonify
+from werkzeug.exceptions import NotFound, BadRequest, InternalServerError
 from .models import db
 from .views import bp
 
@@ -10,4 +11,17 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     app.register_blueprint(bp)
+
+    @app.errorhandler(NotFound)
+    def handle_not_found(e):
+        return jsonify('Not found'), 404
+
+    @app.errorhandler(BadRequest)
+    def handle_bad_request(e):
+        return jsonify('Bad request'), 400
+
+    @app.errorhandler(InternalServerError)
+    def handle_not_found(e):
+        return jsonify('Internal server error'), 500
+
     return app
